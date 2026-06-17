bb93404f0efe3025caa4ad887b98216f40c228de
aspiredream25 <aspiredream25@gmail.com>
2026-06-17 10:17:37 +0530

    Initial commit with backend env secured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from './supabaseClient'; 
-import Login from './Login';
+import Login from './login';
 import AdminDashboard from './Admin';
 import EmployeeDashboard from './EmployeeDashboard';
 import ClientPortal from './ClientPortal';
